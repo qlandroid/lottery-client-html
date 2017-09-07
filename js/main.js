f83b5638c6e378d2c1d461418d09493def6936f7
Liang Qiu <strive_bug@yeah.net>
2017-09-07 18:04:37 +0800

    更改主页显示方式

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ $(document).ready(function () {
         var layer = layui.layer;
         var laytpl = layui.laytpl;
         var url = base + "/a/fill/list";
+
         /**
          * {"code":200,"total":1,"list":
          * [{"lotteryFillOpenId":8,"lotteryFillCreaterDate":1503814720000,"
@@ -15,34 +16,67 @@ $(document).ready(function () {
          * "lotteryRule":"积分数量满足11积分即进行抽奖，中奖用户返还10积分。",
          * "lotteryType":"0","buyQty":1}]}
          */
-        $.post(url, {
-            page:1,
-            pageSize:20,
-            lotteryTypeId:10
-        }, function (d) {
+        function reloadLotteryList() {
+            $.post(url, {
+                page: 1,
+                pageSize: 20,
+                lotteryTypeId: $("#lotteryList").attr("typeId"),
+                lotteryType: $("#lotteryList").attr("type")
+            }, function (d) {
+                if (d.code == 200) {
+                    var list = d.list;
+                    var innerHtml = "";
+                    for (var i = 0; i < list.length; i++) {
+                        console.log(list[i]);
+                        laytpl(fillItem.innerHTML).render(list[i], function (html) {
+                            innerHtml = innerHtml + html;
+                        })
+                    }
+                    $(".lottery-list").html(innerHtml);
+                }
+            });
+        }
+        reloadLotteryList();
+
+        //获得所有的Type
+        var typeListUrl = base + "/lottery/type/all";
+        $.post(typeListUrl, null, function (d) {
             if (d.code == 200) {
+                console.log(JSON.stringify(d));
                 var list = d.list;
                 var innerHtml = "";
                 for (var i = 0; i < list.length; i++) {
                     console.log(list[i]);
-                    laytpl(fillItem.innerHTML).render(list[i], function (html) {
+                    laytpl(typeItem.innerHTML).render(list[i], function (html) {
                         innerHtml = innerHtml + html;
                     })
                 }
-                $(".lottery-list").html(innerHtml);
+                $(".type-list").html(innerHtml);
+            } else {
+                layer.msg(d.code + "----" + d.message);
             }
-        })
+        });
+        //点击type，查看彩票类型下的彩票
+        $(".btn-type-item").on('click', function () {
+            alert("被点击了");
+            var typeId = $(this).data('id');
+            var type = $(this).data('type');
+            $("#lotteryList").attr("typeId", typeId);
+            $("#lotterylist").attr("type", type);
+            //让lotteryList重新加载
+            reloadLotteryList();
+        });
         var token = getCookie("token");
-        if(token){
-            $.post(base+"/user/manager/details",{
-                token:token
-            },function(d){
-                if(d.code == 200){
+        if (token) {
+            $.post(base + "/user/manager/details", {
+                token: token
+            }, function (d) {
+                if (d.code == 200) {
                     var name = d.data.name;
                     var headerLoginHtml = "欢迎用户";
-                    if(!name){
+                    if (!name) {
                         name = "……"
-                    }else{
+                    } else {
 
                     }
 
@@ -60,7 +94,7 @@ $(document).ready(function () {
     })
 });
 
-function outLogin(){
+function outLogin() {
     delCookie("token");
     window.location.reload();
 }
